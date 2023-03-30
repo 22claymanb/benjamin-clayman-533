df4be0f2f58cba4de9941d9c1d228594ea871670
Ben Clayman <benclayman@Bens-MacBook-Pro-4.local>
2023-03-30 10:33:06 -0400

    finished functionality

--- HW3/hw3_traitors.py
+++ HW3/hw3_traitors.py
@@ -3,27 +3,36 @@ import numpy as np
 import math
 
 def blotter_to_ledger(blotter):
-    blotter_df = pd.read_csv(blotter)
-    blotter_df['date'] = pd.to_datetime(blotter_df['date'])
+    blotter['date'] = pd.to_datetime(blotter['date'])
     
     ledger_df = pd.DataFrame(columns=['trade_id', 'asset', 'dt_enter', 'dt_exit', 'success', 'n', 'rtn'])
     for i in set(blotter_df['trade_id']):
-        trade_blotter = blotter_df[blotter_df['trade_id'] == i]
+        trade_blotter = blotter[blotter['trade_id'] == i]
         
         trade_id = i
         asset = trade_blotter.iloc[0]['asset']
         dt_enter = trade_blotter.iloc[0]['date']
+        
         dt_exit = trade_blotter.iloc[-1]['date']
         
         n = (dt_exit - dt_enter).days + 1
+        
         success = 0
         rtn = 0.0
         
         if trade_blotter.iloc[-1]['trip'] != 'ENTER':
             rtn = math.log(trade_blotter.iloc[-1]['price'] / trade_blotter.iloc[0]['price']) / n
             
-            if rtn > 0:
-                success = 1
+            success = 1
+            
+            if trade_blotter.iloc[-1]['type'] == 'MKT':
+                success = -1
+                
+        if trade_blotter.iloc[-1]['status'] == 'LIVE':
+            dt_exit = np.nan
+            success = np.nan
+            n = np.nan
+            rtn = np.nan
                 
                 
         trade_ledger_dict = {
@@ -34,13 +43,15 @@ def blotter_to_ledger(blotter):
             'success' : [success],
             'n' : [n],
             'rtn' : [rtn],
-         }
+        }         
+    
         trade_ledger_df = pd.DataFrame(data=trade_ledger_dict)
-                
-        ledger_df = pd.concat([ledger_df, trade_ledger_df], ignore_index=True)         
-            
+        ledger_df = pd.concat([ledger_df, trade_ledger_df], ignore_index=True)
+        
+        
     return ledger_df
 
-ledger_df = blotter_to_ledger('blotter.csv')
+blotter_df = pd.read_csv('blotter.csv')
+ledger_df = blotter_to_ledger(blotter_df)
 print(ledger_df.head(25))
     
