d2d7faf4409629c81142e16b58335ad250fd48d9
Ben Clayman <benclayman@Bens-MacBook-Pro-4.local>
2023-05-02 14:41:38 -0400

    changed bounds for hoeffding

--- HW4/hoeffding.py
+++ HW4/hoeffding.py
@@ -3,5 +3,5 @@ import numpy as np
 
 
 def apply_hoeffding(n, t):
-    bound = math.exp((-2 * n * (t ** 2)) / ((.005 + .005) ** 2))
+    bound = math.exp((-2 * n * (t ** 2)) / ((.01 + .01) ** 2))
     return bound
